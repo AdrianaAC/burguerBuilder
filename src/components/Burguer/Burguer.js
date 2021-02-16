2b68404be60b3bb2cb273a77c41da06f8ca2f7c4
Adriana <adrianaalves098@gmail.com>
2021-02-16 04:25:07 +0000

    ingredients passed thrpugh state

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -1,14 +1,18 @@
 import React from "react";
 import classes from "./Burguer.css";
-import BurguerIngredient from "./BurguerIngredient/BurguerIngredient"
+import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
 
 const burguer = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+           return <BurguerIngredient key={igKey+i} type={igKey} />
+        });
+    }); 
     return(
         <div className={classes.Burguer}>
             <BurguerIngredient type="bread-top" />
-            <BurguerIngredient type="cheese" />
-            <BurguerIngredient type="meat" />
-            <BurguerIngredient type="bacon" />
+            {transformedIngredients}
             <BurguerIngredient type="bread-bottom" />
 
         </div>
