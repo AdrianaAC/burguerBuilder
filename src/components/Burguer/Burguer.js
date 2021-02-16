55aa348721bd6e530d85c3cc271314d9085cecb5
Adriana <adrianaalves098@gmail.com>
2021-02-16 05:46:58 +0000

    please, add ingredients

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -3,12 +3,19 @@ import classes from "./Burguer.css";
 import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
 
 const burguer = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
            return <BurguerIngredient key={igKey+i} type={igKey} />
         });
-    }); 
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    },[]);
+
+if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please, add ingredients.</p>
+
+}
     return(
         <div className={classes.Burguer}>
             <BurguerIngredient type="bread-top" />
