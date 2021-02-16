ae9ef2e4ed02c98822c39ed32330fccb74f0edef
Adriana <adrianaalves098@gmail.com>
2021-02-16 04:05:35 +0000

    burguer graphic image (hardcoded)

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -1,1 +1,18 @@
 import React from "react";
+import classes from "./Burguer.css";
+import BurguerIngredient from "./BurguerIngredient/BurguerIngredient"
+
+const burguer = (props) => {
+    return(
+        <div className={classes.Burguer}>
+            <BurguerIngredient type="bread-top" />
+            <BurguerIngredient type="cheese" />
+            <BurguerIngredient type="meat" />
+            <BurguerIngredient type="bacon" />
+            <BurguerIngredient type="bread-bottom" />
+
+        </div>
+    );
+};
+
+export default burguer;
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
+import Burguer from "../../components/Burguer/Burguer";
 
 class BurguerBuilder extends Component {
   render() {
     return <Aux>
-        <div>Graphical representation of the burguer</div>
+        <Burguer />
         <div>The kitchen: add and remove ingredients</div>
     </Aux>;
   }
