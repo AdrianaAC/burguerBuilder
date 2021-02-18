ef9c512b57390f36fd26b610d94dc875a84f3558
Adriana <adrianaalves098@gmail.com>
2021-02-18 06:14:17 +0000

    nav bar for smallerscreens

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,13 +4,15 @@ import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
