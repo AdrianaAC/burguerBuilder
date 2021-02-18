eec1552118c9301c208520c7b8764b6119c6a1b9
Adriana <adrianaalves098@gmail.com>
2021-02-18 06:37:15 +0000

    sidedrawer + backdrop working on mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-const layout = (props) => (
-    <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Aux>
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer closed={this.sideDrawerCloseHandler} open={this.state.showSideDrawer}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
-
-);
-
-export default layout;
+export default Layout;
