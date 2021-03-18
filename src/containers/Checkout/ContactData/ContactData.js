2ff37d6cd46485d651ce2a9f02a4d446ba99fc17
Adriana <adrianaalves098@gmail.com>
2021-03-18 01:56:13 +0000

    sharing validity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import { connect } from "react-redux";
 //import axios from "../../../axios-orders";
 //import withErrorHandle from "../../../hoc/withErrorHandle/withErrorHandle";
 import * as actions from "../../../store/actions/index";
-import { updateObject } from "../../../shared/utility";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
 class ContactData extends Component {
   state = {
@@ -115,33 +115,12 @@ class ContactData extends Component {
     this.props.onOrderBurguer(order, this.props.token);
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-
-    if (rules.minLentgh) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    return isValid;
-  }
-
   inputChangeHandler = (event, inputIdentifier) => {
     const updatedFormElement = updateObject(
       this.state.orderForm[inputIdentifier],
       {
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.orderForm[inputIdentifier].validation
         ),
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -1,6 +1,27 @@
 export const updateObject = (oldObject, updatedProperties) => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    };
+  return {
+    ...oldObject,
+    ...updatedProperties,
+  };
+};
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+
+  if (!rules) {
+    return true;
+  }
+
+  if (rules.required) {
+    isValid = value.trim() !== "" && isValid;
+  }
+
+  if (rules.minLentgh) {
+    isValid = value.length >= rules.minLength && isValid;
+  }
+
+  if (rules.maxLength) {
+    isValid = value.length <= rules.maxLength && isValid;
+  }
+  return isValid;
 };
